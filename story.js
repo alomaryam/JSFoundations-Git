84864ac55039b817c691b095ac64efc1e59bcb1a
alomaryam <alomaryam@gmail.com>
2021-02-02 11:59:52 +0300

    task 2 done

--- story.js
+++
@@ -1,5 +0,0 @@
-let client = "Maryam";
-let age = 24;
-let hobby = "painting";
-
-console.log (`Mr./Mrs. ${client} in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
